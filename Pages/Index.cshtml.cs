56c40e770a75c54a39e511db33cd90257a6e68a0
Peter Keenan <keenan@posteo.net>
2020-10-26 21:59:04 +0000

    Capitalised namespace

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace peterkeenan.Pages
+namespace PeterKeenan.Pages
 {
     public class IndexModel : PageModel
     {
